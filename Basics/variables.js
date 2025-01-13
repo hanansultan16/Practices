86edc9b0328e5a9a87ac29df0e81a323236f4cfc
Hanan Sultan <hanan.bhatti.39@gmail.com>
2025-01-13 10:24:55 +0500

    JavaScript Practices

---
+++ Basics/variables.js
@@ -0,0 +1,69 @@
+// 1. Store Personal Information
+let firstName = "Hanan";
+let lastName = "Sultan";
+let age = 27;
+let country = "Pakistan";
+
+console.log(`My name is ${firstName} ${lastName}. I am ${age} years old and I live in ${country}.`);
+
+
+// 2. Shopping Cart
+let appleCost = 2;
+let bananaCost = 1;
+let mangoCost = 3;
+let totalCost = appleCost + bananaCost + mangoCost;
+
+console.log(`Total Cost of fruits is ${totalCost}$.`);
+
+
+// 3. Temperature Converter
+let celsius = 12;
+let fahrenheit = (celsius * 9/5) + 32
+
+console.log(fahrenheit);
+
+
+// 4. Event Budget Calculator
+let numberOfGuests = 400;
+let costPerPerson = 500;
+let totalBudget = 300000;
+let eventCost = numberOfGuests * costPerPerson;
+
+console.log(`The total event cost will be: ${eventCost}.`);
+
+
+// 5. Simple Loan Calculator
+let loanAmount = 50000;
+let interestRate = 2;
+let loanTerm = 3;
+let totalInterest = loanAmount * (interestRate / 100) * loanTerm;
+
+console.log(`The total in interest in loan will be: ${totalInterest}.`);
+
+
+// 6. Fuel Efficiency
+let totalDistance= 90;
+let fuelConsumned = 10;
+let fuelEfficiency = totalDistance/ fuelConsumned;
+
+console.log(fuelEfficiency);
+
+
+// 7. Restaurant Tip Calculator
+let totalBill = 30000;
+let tip = totalBill * 0.15;
+
+console.log(tip);
+
+
+// 8. Birthday Countdown
+let daysInYear = 365;
+let daysUntilBirthday = 150;
+
+console.log(`Days until your next birthday: ${daysUntilBirthday}.`)
+
+
+
+
+
+
